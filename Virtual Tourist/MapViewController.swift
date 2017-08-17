3b84085046988f2f96030a271903358e743fc4be
Jamel Reid <jreid@qualityworkscg.com>
2017-08-17 10:37:03 -0500

    crated the intial UI layout

---
+++ Virtual Tourist/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  Virtual Tourist
+//
+//  Created by Jamel Reid  on 8/17/17.
+//  Copyright © 2017 Jamel Reid . All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
